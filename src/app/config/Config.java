57a46ce809d188b3ec82f2e88c8ba7b5c2b7bb79
Dmitry Vlasenko <57891870+Dmitry-Vlasenko@users.noreply.github.com>
2024-02-27 15:06:07 +0200

    add config

---
+++ src/app/config/Config.java
@@ -0,0 +1,7 @@
+package app.config;
+
+
+public class Config {
+    static final public String[] fruitList = {"Apple", "Banana", "Orange", "Strawberry", "Grape", "Watermelon", "Pineapple", "Mango", "Kiwi", "Blueberry"};
+    static final public String[] vegetableList = {"Carrot", "Broccoli", "Spinach", "Tomato", "Cucumber", "Bell Pepper", "Zucchini", "Lettuce", "Onion", "Potato"};
+}
